2b78ed8278d8628c39a285bad74f0aa29ee74034
Clement <clement.roelens@hotmail.com>
2024-12-04 13:44:52 +0100

    updated

--- src/main/java/ConfigurationManager.java
+++ src/main/java/ConfigurationManager.java
@@ -1,31 +1,37 @@
 import java.util.Map;
 
 public class ConfigurationManager {
-    private String hostname;
+//    private String hostname;
     // Pour simplifier j'ai mis un String pour le port
-    private String port;
-    private String appname;
+//    private String port;
+//    private String appname;
+    private Map<String,String> config;
     private static ConfigurationManager instance;
 
-    private ConfigurationManager(String hostname, String port, String appname) {
-        this.hostname = hostname;
-        this.port = port;
-        this.appname = appname;
+    private ConfigurationManager(Map<String,String> config) {
+//        this.hostname = hostname;
+//        this.port = port;
+//        this.appname = appname;
+        this.config = config;
     }
 
-    public static synchronized ConfigurationManager getInstance(Map<String,String> dictionnary) {
+    public static synchronized ConfigurationManager getInstance(Map<String,String> config) {
         if (instance == null) {
-            instance = new ConfigurationManager(dictionnary.get("hostname"), dictionnary.get("port"), dictionnary.get("appname"));
+            instance = new ConfigurationManager(config);
         }
         return instance;
     }
 
-    @Override
-    public String toString() {
-        return String.format("La configuration est : \n" +
-                "hostname : %s\n" +
-                "port : %s\n" +
-                "appname : %s",
-                hostname, port, appname);
+//    @Override
+//    public String toString() {
+//        return String.format("La configuration est : \n" +
+//                "hostname : %s\n" +
+//                "port : %s\n" +
+//                "appname : %s",
+//                hostname, port, appname);
+//    }
+
+    public String giveValue(String key){
+        return config.get(key);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,9 @@ public class Main {
 
         System.out.println("\nInitialisation de la configuration\n");
         ConfigurationManager config = ConfigurationManager.getInstance(dictionnary);
-        System.out.println(config);
+        System.out.println("Valeur de hostname : " + config.giveValue("hostname"));
+        System.out.println("Valeur de port : " + config.giveValue("port"));
+        System.out.println("Valeur de appname : " + config.giveValue("appname"));
 
         System.out.println("\nMaintenant, retentons avec un autre dictionnaire");
 
@@ -19,7 +21,9 @@ public class Main {
         dictionnaryBis.put("appname","genhgernjigerg");
 
         System.out.println("Tentative d'initialisation de la seconde configuration\n");
-        ConfigurationManager configBis = ConfigurationManager.getInstance(dictionnary);
-        System.out.println(configBis);
+        ConfigurationManager configBis = ConfigurationManager.getInstance(dictionnaryBis);
+        System.out.println("Valeur de hostname : " + configBis.giveValue("hostname"));
+        System.out.println("Valeur de port : " + configBis.giveValue("port"));
+        System.out.println("Valeur de appname : " + configBis.giveValue("appname"));
     }
 }
