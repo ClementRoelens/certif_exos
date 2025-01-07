6e9ec6369db4e15b902988b119ffcc47c599da45
Clement <clement.roelens@hotmail.com>
2025-01-07 16:07:34 +0100

    fini sans bonus

---
+++ src/main/java/Journal.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.stream.Collectors;
+
+public class Journal {
+    private final String txtPath;
+    private final String binaryPath;
+    private final File file;
+
+    public Journal(String path, String binaryPath) {
+        this.txtPath = path;
+        this.binaryPath = binaryPath;
+        this.file = new File(path);
+    }
+
+    public String getBinaryPath() {
+        return binaryPath;
+    }
+
+    private void createFileIfNotExists() throws IOException {
+        if (!file.exists()) {
+            Files.createFile(Path.of(txtPath));
+        }
+    }
+
+    public void addActivity(String activity) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
+            String activityLine = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + " - " + activity;
+            writer.write(activityLine);
+            writer.newLine();
+        } catch (FileNotFoundException e) {
+            createFileIfNotExists();
+        }
+    }
+
+    public String getActivities() throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(txtPath))){
+            return reader.lines()
+                    .collect(Collectors.joining("\n"));
+        }
+    }
+
+    public void saveBinary() throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(file));
+             BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(binaryPath)))
+        {
+            for (String line : reader.lines().toArray(String[]::new)){
+                outputStream.write(line.getBytes(StandardCharsets.UTF_8));
+                outputStream.write(System.lineSeparator().getBytes(StandardCharsets.UTF_8));
+            }
+        }
+    }
+
+    public String readBinary() throws IOException {
+        try (BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(binaryPath))){
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            StringBuilder result = new StringBuilder();
+
+            while ((bytesRead = inputStream.read(buffer)) > 0){
+                result.append(new String(buffer, 0, bytesRead));
+            }
+
+            return result.toString();
+        }
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,76 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Journal journal = new Journal("journal.txt", "journal_backup.dat");
+        Scanner IRM = new Scanner(System.in);
+        int choice = 0;
+
+        while (choice != 5){
+            while (choice == 0){
+                System.out.print("""
+                --- Menu ---
+                1. Ajouter une activité
+                2. Afficher les activités
+                3. Sauvegarder en binaire
+                4. Lire le fichier binaire
+                5. Quitter
+                Choisissez une option :
+                """);
+                try {
+                    choice = IRM.nextInt();
+                } catch (InputMismatchException e){
+                    System.out.println("Saisissez un des nombres proposés");
+                } finally {
+                    IRM.nextLine();
+                }
+            }
+
+            switch (choice){
+                case 1 -> {
+                    System.out.print("Entrez une description de l'activité : ");
+                    try {
+                        journal.addActivity(IRM.nextLine());
+                    } catch (IOException e) {
+                        System.err.println(e.getMessage());
+                    }
+                }
+                case 2 -> {
+                    System.out.println("""
+                        --- Journal des activités ---
+                        """);
+                    try {
+                        System.out.println(journal.getActivities());
+                    } catch (IOException e) {
+                        System.err.println(e.getMessage());
+                    }
+                }
+                case 3 -> {
+                    try {
+                        journal.saveBinary();
+                        System.out.println("La sauvegarde a été faite dans : " + journal.getBinaryPath());
+                    } catch (IOException e) {
+                        System.err.println("La sauvegarde n'a pas pu être faite\n" + e.getMessage());
+                    }
+                }
+                case 4 -> {
+                    try {
+                        System.out.println("--- Contenu du fichier binaire ---");
+                        System.out.println(journal.readBinary());
+                    } catch (IOException e) {
+                        System.err.println(e.getMessage());
+                    }
+                }
+            }
+
+            if (choice != 5){
+                choice = 0;
+            }
+        }
+    }
+}
