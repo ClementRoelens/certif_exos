f054487fbd98144f4cff153a7cf0859a6c3f5d77
ClementRoelens <clement.roelens@hotmail.com>
2025-01-09 09:05:50 +0100

    fini

--- src/main/java/MonsterUtils.java
+++ src/main/java/MonsterUtils.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 import java.util.Random;
 
 public abstract class MonsterUtils {
@@ -9,17 +10,13 @@ public abstract class MonsterUtils {
     public static Character getRandomMonster() throws IOException {
         try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/monsters.txt"))) {
             Random random = new Random();
-            String monsterLine = "";
-            long limit = reader.lines().count();
-
-            for (int i = 0; i < random.nextInt()*limit; i++) {
-                monsterLine = reader.readLine();
-            }
+            List<String> monsters = reader.lines().toList();
+            String monster = monsters.get(random.nextInt(0, monsters.size()));
 
             return new Character(
-                    monsterLine.split(",")[0],
-                    Integer.parseInt(monsterLine.split(",")[1].trim()),
-                    Integer.parseInt(monsterLine.split(",")[2].trim())
+                    monster.split(",")[0],
+                    Integer.parseInt(monster.split(",")[1].trim()),
+                    Integer.parseInt(monster.split(",")[2].trim())
             );
         }
     }
